b3e73f18f8a2dc9dc235d56dcbe1f2dcd3be880f
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-03-03 01:39:36 +0100

    Lesson vararg, default parameters and named arguments

--- src/arguments/MainArguments.kt
+++ src/arguments/MainArguments.kt
@@ -11,8 +11,24 @@ fun sayHelloVararg(greeting: String, vararg itemsToGreet: String) {
     }
 }
 
+// Default arguments are used if no arguments are passed to the function during the call
+fun greetPerson(greeting: String = "Hello", person: String = "Kotlin") = println("$greeting $person")
+
 fun main() {
-    val itemsToGreet = listOf("Vittorio", "World", "Giovanni")
-    sayHello("Hello", itemsToGreet)
+    val itemsToGreet1 = listOf("Vittorio", "World", "Giovanni")
+    sayHello("Hello", itemsToGreet1)
+    println("----------------------------------")
+
+    // The arguments of vararg itemsToGreat will be stored inside an Array
     sayHelloVararg("Good morning", "Matteo", "Giovanni", "Alberto", "Giulia")
+    println("----------------------------------")
+
+    // During compilation, it is not likely that the values we need to pass to a function are in the form of an array, so in order to pass them to our vararg parameter we can use the spread operator
+    val itemsToGreet2 = arrayOf("Python", "Kotlin", "Javascript")
+    sayHelloVararg("Ciao", *itemsToGreet2)
+    println("----------------------------------")
+
+    // Named arguments are useful if you need to mix the order of the arguments
+    greetPerson(person = "Vittorio", greeting = "Hi")
+    greetPerson(person = "Giovanni")
 }
